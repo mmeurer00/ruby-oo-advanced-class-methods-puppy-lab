9b29add7b3b4df376c9870cb9d42efe9ac0d46e9
Maxine <maxinemeurer@yahoo.com>
2021-02-13 13:35:33 -0500

    commit tree 54de137532df26cd93db426ebcc349993c67f233

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog
+    attr_accessor :name
+
+    @@all = []
+    @@dog_names = []
+
+    def initialize(name)
+        @name = name
+        @@dog_names << name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+       @@all.each do |obj|
+            puts obj.name
+       end
+    end
+
+    def name
+        @name
+    end
+
+    def save
+        @@all << self
+    end 
+end
